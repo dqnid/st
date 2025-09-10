d5701d30bd71a823e4d6a048e034c1b04ccb20d6
Daniel Heras Quesada <dani.heras@hotmail.com>
2025-09-10 22:09:58 +0200

    feat: emoji support added

--- config.h
+++ config.h
@@ -6,6 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "JetBrainsMono NFP:pixelsize=16:antialias=true:autohint=true";
+static char *font2[] = { "JoyPixels:pixelsize=16:antialias=true:autohint=true" };
 static int borderpx = 22;
 
 /*
